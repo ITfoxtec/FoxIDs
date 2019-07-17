30aa96d6e894da4a08edcc8ad338646547598b7d
Anders Revsgaard <anders@itfoxtec.com>
2019-07-17 10:55:12 +0200

    PublishProfile=Release

--- deploy.cmd
+++ deploy.cmd
@@ -73,7 +73,7 @@ call :ExecuteCmd dotnet restore "%PROJECT%"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. Build and publish 
-call :ExecuteCmd dotnet publish "%PROJECT%" --output "%DEPLOYMENT_TEMP%" --configuration Release
+call :ExecuteCmd dotnet publish "%PROJECT%" --output "%DEPLOYMENT_TEMP%" --configuration Release /p:PublishProfile=Release
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 3. KuduSync

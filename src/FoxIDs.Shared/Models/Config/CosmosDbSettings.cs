419a551f84d250eadc67865962f3b1c855c25f20
Anders Revsgaard <anders@itfoxtec.com>
2019-08-12 10:47:56 +0200

    config validation

--- src/FoxIDs.Shared/Models/Config/CosmosDbSettings.cs
+++ src/FoxIDs.Shared/Models/Config/CosmosDbSettings.cs
@@ -1,17 +1,33 @@
-﻿namespace FoxIDs.Models.Config
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoxIDs.Models.Config
 {
     public class CosmosDbSettings
     {
+        /// <summary>
+        /// Cosmos DB endpoint.
+        /// </summary>
+        [Required]
         public string EndpointUri { get; set; }
+        /// <summary>
+        /// Cosmos DB primary key.
+        /// </summary>
+        [Required]
         public string PrimaryKey { get; set; }
+        /// <summary>
+        /// Default database Id.
+        /// </summary>
+        [Required]
         public string DatabaseId { get; set; }
         /// <summary>
         /// Default Collection Id.
         /// </summary>
+        [Required]
         public string CollectionId { get; set; }
         /// <summary>
         /// Time-to-live (TTL) Collection Id.
         /// </summary>
+        [Required]
         public string TtlCollectionId { get; set; }
     }
 }
--- tools/FoxIDs.SeedDataTool/Model/SeedSettings.cs
+++ tools/FoxIDs.SeedDataTool/Model/SeedSettings.cs
@@ -1,25 +1,74 @@
-﻿using UrlCombineLib;
+﻿using FoxIDs.Models.Config;
+using System.ComponentModel.DataAnnotations;
+using UrlCombineLib;
 
 namespace FoxIDs.SeedDataTool.Model
 {
     public class SeedSettings
     {
+        /// <summary>
+        /// Seed tool client id.
+        /// </summary>
         public string ClientId => DownParty;
+        /// <summary>
+        /// Seed tool client secret.
+        /// </summary>
+        [Required]
         public string ClientSecret { get; set; }
 
+        /// <summary>
+        /// Seed tool redirect uri.
+        /// </summary>
+        [Required]
         public string RedirectUri { get; set; }
 
-        public string Authority => FoxIDsEndpoint == null ? null : UrlCombine.Combine(FoxIDsEndpoint, Tenant, Track, DownParty);
+        public string Authority => UrlCombine.Combine(FoxIDsEndpoint, Tenant, Track, DownParty);
 
+        /// <summary>
+        /// FoxIDs endpoint.
+        /// </summary>
+        [Required]
         public string FoxIDsEndpoint { get; set; }
+        /// <summary>
+        /// Seed tool tenant.
+        /// </summary>
+        [Required]
         public string Tenant { get; set; }
+        /// <summary>
+        /// Seed tool track.
+        /// </summary>
+        [Required]
         public string Track { get; set; }
+        /// <summary>
+        /// Seed tool down party (client id).
+        /// </summary>
+        [Required]
         public string DownParty { get; set; }
 
+        /// <summary>
+        /// FoxIDs api endpoint.
+        /// </summary>
+        [Required]
         public string FoxIDsApiEndpoint { get; set; }
+        /// <summary>
+        /// FoxIDs master api endpoint.
+        /// </summary>
         public string FoxIDsMasterApiEndpoint => UrlCombine.Combine(FoxIDsApiEndpoint, "@master");
+        /// <summary>
+        /// FoxIDs master track api endpoint.
+        /// </summary>
         public string FoxIDsMasterTrackApiEndpoint => UrlCombine.Combine(FoxIDsApiEndpoint, "master");
 
-        public string PwnedPasswordsSha1OrderedByCountPath { get; set; }
+        /// <summary>
+        /// Pwned passwords (SHA1 ordered by count) path.
+        /// </summary>
+        [Required]
+        public string PwnedPasswordsPath { get; set; }
+
+        /// <summary>
+        /// Cosmos DB configuration.
+        /// </summary>
+        [Required]
+        public CosmosDbSettings CosmosDb { get; set; }
     }
 }

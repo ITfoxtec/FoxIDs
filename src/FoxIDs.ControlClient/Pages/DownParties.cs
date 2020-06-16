18dbf7c944d345e99c797aa6fa991c591092e28d
Anders Revsgaard <anders@itfoxtec.com>
2020-06-16 10:06:10 +0200

    Up and down parties, seperat file and url.

---
+++ src/FoxIDs.ControlClient/Pages/DownParties.cs
@@ -0,0 +1,80 @@
+﻿using FoxIDs.Client.Infrastructure;
+using FoxIDs.Client.Logic;
+using FoxIDs.Models.Api;
+using FoxIDs.Client.Models.ViewModels;
+using FoxIDs.Client.Services;
+using FoxIDs.Client.Shared.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoxIDs.Client.Pages
+{
+    public partial class DownParties
+    {
+        private PageEditForm<FilterPartyViewModel> downPartyFilterForm;
+        private IEnumerable<DownParty> downParties = new List<DownParty>();
+        private Modal downPartyModal;
+        private string upPartyHref;
+
+        [Inject]
+        public RouteBindingLogic RouteBindingLogic { get; set; }
+
+        [Inject]
+        public NotificationLogic NotificationLogic { get; set; }
+
+        [Inject]
+        public DownPartyService DownPartyService { get; set; }
+
+        [Inject]
+        public UpPartyService UpPartyService { get; set; }
+
+        [Parameter]
+        public string TenantName { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            upPartyHref = $"{await RouteBindingLogic.GetTenantNameAsync()}/upparties";
+            await base.OnInitializedAsync();
+            await DefaultLoadAsync();
+        }
+
+        private async Task DefaultLoadAsync()
+        {
+            try
+            {
+                downParties = await DownPartyService.FilterDownPartyAsync(null);
+            }
+            catch (Exception ex)
+            {
+                downPartyFilterForm.SetError(ex.Message);
+            }
+        }
+
+        private async Task OnDownPartyFilterValidSubmitAsync(EditContext editContext)
+        {
+            try
+            {
+                downParties = await DownPartyService.FilterDownPartyAsync(downPartyFilterForm.Model.FilterName);
+            }
+            catch (FoxIDsApiException ex)
+            {
+                if (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    downPartyFilterForm.SetFieldError(nameof(downPartyFilterForm.Model.FilterName), ex.Message);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task ShowDownPartyAsync(string upPartyName)
+        {
+
+        }
+    }
+}
---
+++ src/FoxIDs.ControlClient/Pages/UpParties.cs
@@ -0,0 +1,80 @@
+﻿using FoxIDs.Client.Infrastructure;
+using FoxIDs.Client.Logic;
+using FoxIDs.Models.Api;
+using FoxIDs.Client.Models.ViewModels;
+using FoxIDs.Client.Services;
+using FoxIDs.Client.Shared.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoxIDs.Client.Pages
+{
+    public partial class UpParties
+    {
+        private PageEditForm<FilterPartyViewModel> upPartyFilterForm;
+        private IEnumerable<UpParty> upParties = new List<UpParty>();
+        private Modal upPartyModal;
+        private string downPartyHref;
+
+        [Inject]
+        public RouteBindingLogic RouteBindingLogic { get; set; }
+
+        [Inject]
+        public NotificationLogic NotificationLogic { get; set; }
+
+        [Inject]
+        public DownPartyService DownPartyService { get; set; }
+
+        [Inject]
+        public UpPartyService UpPartyService { get; set; }
+
+        [Parameter]
+        public string TenantName { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            downPartyHref = $"{await RouteBindingLogic.GetTenantNameAsync()}/downparties";
+            await base.OnInitializedAsync();
+            await DefaultLoadAsync();
+        }
+
+        private async Task DefaultLoadAsync()
+        {
+            try
+            {
+                upParties = await UpPartyService.FilterUpPartyAsync(null);
+            }
+            catch (Exception ex)
+            {
+                upPartyFilterForm.SetError(ex.Message);
+            }
+        }
+        
+        private async Task OnUpPartyFilterValidSubmitAsync(EditContext editContext)
+        {
+            try
+            {
+                upParties = await UpPartyService.FilterUpPartyAsync(upPartyFilterForm.Model.FilterName);
+            }
+            catch (FoxIDsApiException ex)
+            {
+                if (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    upPartyFilterForm.SetFieldError(nameof(upPartyFilterForm.Model.FilterName), ex.Message);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task ShowUpPartyAsync(string upPartyName)
+        {
+
+        }
+    }
+}

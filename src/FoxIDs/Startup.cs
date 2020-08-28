1cad125c69e0956c1f9630cc45f02911964b6cfb
Anders Revsgaard <anders@itfoxtec.com>
2020-08-28 09:09:32 +0200

    Azure Deployment parms moved.

--- src/FoxIDs/Startup.cs
+++ src/FoxIDs/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+Problem deployment parameter
+
 namespace FoxIDs
 {
     public class Startup

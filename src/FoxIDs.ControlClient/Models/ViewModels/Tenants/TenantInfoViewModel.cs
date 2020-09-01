c4d4fae76e884694b700e24f0d09cb2858597e34
Anders Revsgaard <anders@itfoxtec.com>
2020-09-01 15:23:33 +0200

    Client Down/Up party not update name. Add FFieldText.razor.

--- src/FoxIDs.ControlClient/Models/ViewModels/Tenants/TenantInfoViewModel.cs
+++ src/FoxIDs.ControlClient/Models/ViewModels/Tenants/TenantInfoViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace FoxIDs.Client.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoxIDs.Client.Models.ViewModels
 {
     public class TenantInfoViewModel
     {
+        [Display(Name = "Tenant name")]
         public string Name { get; set; }
 
         public string LoginUri { get; set; }

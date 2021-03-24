2225cbd52f20049ebb04b5a3d51fe451b0a54667
Anders Revsgaard <anders@itfoxtec.com>
2021-03-24 21:54:33 +0100

    Nuget update Client up-party OIDC require issuer problem resolved.

--- src/FoxIDs/Extensions/HtmActionExtensions.cs
+++ src/FoxIDs/Extensions/HtmActionExtensions.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 namespace FoxIDs
 {
     /// <summary>
-    /// Extension methods for ActionResult and dictionarys.
+    /// Extension methods for HTML form and redirect actions.
     /// </summary>
-    public static class ListActionResultExtensions
+    public static class HtmActionExtensions
     {
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a HTML Post Content Result.
@@ -21,6 +21,19 @@ namespace FoxIDs
                 Content = items.ToHtmlPostPage(url),
             });
         }
+
+        /// <summary>
+        /// Converts a url to a Redirect Result.
+        /// </summary>
+        public static Task<ContentResult> ToRedirectResultAsync(this string url)
+        {
+            return Task.FromResult(new ContentResult
+            {
+                ContentType = "text/html",
+                Content = HtmRedirectActionPageList(url),
+            });
+        }
+
 
         /// <summary>
         /// Converts a Dictionary&lt;string, string&gt; to a Redirect Result.
@@ -44,6 +57,25 @@ namespace FoxIDs
                 ContentType = "text/html",
                 Content = items.ToHtmlFragmentPage(url),
             });
+        }
+
+        private static string HtmRedirectActionPageList(string url)
+        {
+            return
+$@"<!DOCTYPE html>
+<html lang=""en"">
+    <head>
+        <meta charset=""utf-8"" />
+        <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
+        <meta http-equiv=""refresh"" content=""0;URL='{url}'"" />
+        <title>OAuth 2.0</title>
+    </head>
+    <body>
+        <p>
+            Please press <a href=""{url}"">here</a> to continue the proceed.
+        </p>
+    </body>
+</html>";
         }
     }
 }
---
+++ src/FoxIDs/Extensions/RouteBindingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FoxIDs.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace FoxIDs
+{
+    public static class RouteBindingExtensions
+    {
+        public static string GetRouteSequenceString(this HttpContext httpContext)
+        {
+            return httpContext.Items[Constants.Routes.SequenceStringKey] as string;
+        }
+
+        public static string ToUpPartyBinding(this string upPartyName, PartyBindingPatterns partyBindingPattern)
+        {
+            return partyBindingPattern switch
+            {
+                PartyBindingPatterns.Brackets => $"({upPartyName})",
+                PartyBindingPatterns.Tildes => $"~{upPartyName}~",
+                _ => throw new NotImplementedException($"Party binding pattern '{partyBindingPattern}' not implemented.")
+            };
+        }
+    }
+}

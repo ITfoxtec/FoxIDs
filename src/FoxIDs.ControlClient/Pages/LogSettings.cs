5fe9ad3445b35bf409da84104178be9886b5a019
Anders Revsgaard <anders@itfoxtec.com>
2021-04-20 22:02:37 +0200

    Client, add logs page and remove headlines.

---
+++ src/FoxIDs.ControlClient/Pages/LogSettings.cs
@@ -0,0 +1,26 @@
+﻿using FoxIDs.Client.Logic;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxIDs.Client.Pages
+{
+    public partial class LogSettings
+    {
+        private string logsHref;
+
+        [Inject]
+        public RouteBindingLogic RouteBindingLogic { get; set; }
+
+        [Parameter]
+        public string TenantName { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            logsHref = $"{await RouteBindingLogic.GetTenantNameAsync()}/logs";
+            await base.OnInitializedAsync();
+        }
+    }
+}
---
+++ src/FoxIDs.ControlClient/Pages/Logs.cs
@@ -0,0 +1,26 @@
+﻿using FoxIDs.Client.Logic;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxIDs.Client.Pages
+{
+    public partial class Logs
+    {
+        private string logSettingsHref;
+
+        [Inject]
+        public RouteBindingLogic RouteBindingLogic { get; set; }
+
+        [Parameter]
+        public string TenantName { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            logSettingsHref = $"{await RouteBindingLogic.GetTenantNameAsync()}/logsettings";
+            await base.OnInitializedAsync();
+        }
+    }
+}

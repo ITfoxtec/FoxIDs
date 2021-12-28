cccb12cf1d0508a20fe681796d7dbb3f0ecfad3c
Anders Revsgaard <anders@itfoxtec.com>
2021-12-28 11:10:01 +0100

    Control Client production track text.

--- src/FoxIDs.ControlClient/Extensions/TrackExtensions.cs
+++ src/FoxIDs.ControlClient/Extensions/TrackExtensions.cs
@@ -4,11 +4,21 @@ namespace FoxIDs.Client
 {
     public static class TrackExtensions
     {
+        public static string GetProdTrackName(this string trackName)
+        {
+            if ("-".Equals(trackName, StringComparison.Ordinal))
+            {
+                return "(dash is production)";
+            }
+
+            return String.Empty;
+        }
+
         public static string FormatTrackName(this string trackName)
         {
             if ("-".Equals(trackName, StringComparison.Ordinal))
             {
-                return "Production (-)";
+                return "- (dash is production)";
             }
 
             return trackName;

8f79c02f6db9e3f7535a7c79c74faf1069187d97
Anders Revsgaard <anders@itfoxtec.com>
2022-03-02 20:26:01 +0100

    add MfaController

---
+++ src/FoxIDs/Controllers/MfaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using FoxIDs.Infrastructure;
+using FoxIDs.Infrastructure.Filters;
+using FoxIDs.Logic;
+using FoxIDs.Models;
+using FoxIDs.Models.Sequences;
+using FoxIDs.Models.ViewModels;
+using FoxIDs.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace FoxIDs.Controllers
+{
+    [Sequence]
+    public class MfaController : EndpointController
+    {
+        private readonly TelemetryScopedLogger logger;
+        private readonly IStringLocalizer localizer;
+        private readonly ITenantRepository tenantRepository;
+        private readonly SecurityHeaderLogic securityHeaderLogic;
+        private readonly AccountLogic userAccountLogic;
+        private readonly AccountActionLogic accountActionLogic;
+
+        public MfaController(TelemetryScopedLogger logger, IStringLocalizer localizer, ITenantRepository tenantRepository, SecurityHeaderLogic securityHeaderLogic, AccountLogic userAccountLogic, AccountActionLogic accountActionLogic) : base(logger)
+        {
+            this.logger = logger;
+            this.localizer = localizer;
+            this.tenantRepository = tenantRepository;
+            this.securityHeaderLogic = securityHeaderLogic;
+            this.userAccountLogic = userAccountLogic;
+            this.accountActionLogic = accountActionLogic;
+        }
+    }
+}

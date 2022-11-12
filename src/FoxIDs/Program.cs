fd9a790d80bbdde71953e3ea69cbd2e048f29d8e
Anders Revsgaard <anders@itfoxtec.com>
2022-11-12 21:16:55 +0100

    Load of "ApplicationInsights:ConnectionString" improved

--- src/FoxIDs/Program.cs
+++ src/FoxIDs/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Azure.Identity;
-using ITfoxtec.Identity;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Globalization;
 
 namespace FoxIDs
 {
@@ -30,8 +30,8 @@ namespace FoxIDs
                 .UseStartup<Startup>()
                 .ConfigureLogging((context, logging) =>
                 {
-                    var connectionString = context.Configuration.GetSection("ApplicationInsights:ConnectionString").Value;
-                    if (connectionString.IsNullOrWhiteSpace())
+                    var connectionString = context.Configuration.GetSection("ApplicationInsights:ConnectionString")?.Value;
+                    if (string.IsNullOrWhiteSpace(connectionString))
                     {
                         connectionString = ReadInstrumentationKey(context);
                     }
@@ -53,8 +53,15 @@ namespace FoxIDs
         [Obsolete("ApplicationInsights InstrumentationKey is being deprecated. See https://github.com/microsoft/ApplicationInsights-dotnet/issues/2560 for more details.")]
         private static string ReadInstrumentationKey(WebHostBuilderContext context)
         {
-            var instrumentationKey = context.Configuration.GetSection("ApplicationInsights:InstrumentationKey").Value;
-            return $"InstrumentationKey={instrumentationKey}";
+            var instrumentationKey = context.Configuration.GetSection("ApplicationInsights:InstrumentationKey")?.Value;
+            if (string.IsNullOrWhiteSpace(instrumentationKey))
+            {
+                return $"InstrumentationKey={instrumentationKey}";
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }

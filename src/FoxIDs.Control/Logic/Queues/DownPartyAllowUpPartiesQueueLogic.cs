cdfa09c3d4f05266235a0aff73b1869bcfe6d974
Anders Revsgaard <anders@itfoxtec.com>
2022-11-12 20:41:01 +0100

    Background queue use logging settings. TelemetryScopedLogger not logging scope properties in event bug.

--- src/FoxIDs.Control/Logic/Queues/DownPartyAllowUpPartiesQueueLogic.cs
+++ src/FoxIDs.Control/Logic/Queues/DownPartyAllowUpPartiesQueueLogic.cs
@@ -91,6 +91,7 @@ namespace FoxIDs.Logic
             {
                 TenantName = routeBinding.TenantName,
                 TrackName = routeBinding.TrackName,
+                Logging = routeBinding.Logging,
                 LogicClassTypeFullName = GetType().FullName,
                 Info = remove ? $"Remove up-party '{upParty.Name}' from down-parties allow up-party list" : $"Update up-party '{upParty.Name}' in down-parties allow up-party list",
                 Message = message.ToJson(),

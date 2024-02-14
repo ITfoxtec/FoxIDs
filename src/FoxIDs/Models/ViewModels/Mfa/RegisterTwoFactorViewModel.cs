58942008cf7b59f68eec9b83d25fb1f5fbf858fb
Anders Revsgaard <anders@itfoxtec.com>
2024-02-14 16:45:08 +0100

    Improve two factor reg texts

--- src/FoxIDs/Models/ViewModels/Mfa/RegisterTwoFactorViewModel.cs
+++ src/FoxIDs/Models/ViewModels/Mfa/RegisterTwoFactorViewModel.cs
@@ -4,13 +4,11 @@ namespace FoxIDs.Models.ViewModels
 {
     public class RegisterTwoFactorViewModel : ViewModel
     {
-        [Display(Name = "First, scan the QR code with your app")]
         public string QrCodeSetupImageUrl { get; set; }
 
-        [Display(Name = "Or, use manual setup key (optional)")]
         public string ManualSetupKey { get; set; }
 
-        [Display(Name = "Then, enter the authenticator app code")]
+        [Display(Name = "Code")]
         [Required]
         [MaxLength(Constants.Models.User.TwoFactorAppCodeLength, ErrorMessage = "Please enter a valid code")]
         public string AppCode { get; set; }

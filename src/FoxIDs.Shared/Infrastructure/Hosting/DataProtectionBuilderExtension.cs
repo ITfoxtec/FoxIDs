8bb69a36cf1a51fe1e44749b5dc55bd24d06acad
Anders Revsgaard <anders@itfoxtec.com>
2024-06-17 11:01:06 +0200

    Cache data protection also if not using Redis. Error checkin.

---
+++ src/FoxIDs.Shared/Infrastructure/Hosting/DataProtectionBuilderExtension.cs
@@ -0,0 +1,25 @@
+﻿using FoxIDs.Models.Config;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxIDs.Infrastructure.Hosting
+{
+    public static class DataProtectionBuilderExtension
+    {
+        public static IDataProtectionBuilder PersistKeysToCacheNotRedis(this IDataProtectionBuilder builder, CacheOptions cache, string key)
+        {
+            builder.Services.Configure<KeyManagementOptions>(options =>
+            {
+                options.XmlRepository = new DataProtectionCacheNotRedisRepository(key);
+            });
+            return builder;
+        }
+    }
+}

fde3377eab6a76c94d9d4aec20db4b46a182ca32
stas.novogursky <stas.novogursky@invensus.com>
2024-08-23 11:04:52 +0200

    Extend Claims.ValuesMax = 250 to meet Azure max values (200 groups for JSON web tokens - https://learn.microsoft.com/en-us/security/zero-trust/develop/configure-tokens-group-claims-app-roles#group-overages)

--- src/FoxIDs.SharedBase/Constants.cs
+++ src/FoxIDs.SharedBase/Constants.cs
@@ -437,7 +437,7 @@ namespace FoxIDs
 
                 public const int ValuesOAuthMin = 0;
                 public const int ValuesUserMin = 1;
-                public const int ValuesMax = 100;
+                public const int ValuesMax = 250;
 
                 /// <summary>
                 /// JWT and SAML claim value max length.

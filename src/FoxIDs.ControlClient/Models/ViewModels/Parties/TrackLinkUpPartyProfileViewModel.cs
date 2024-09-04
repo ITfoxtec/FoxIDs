0e0083efc79a747fca2862edc9a10d0ee2895d0f
Anders Revsgaard <anders@itfoxtec.com>
2024-09-04 08:19:54 +0200

    adding profiles to client...

---
+++ src/FoxIDs.ControlClient/Models/ViewModels/Parties/TrackLinkUpPartyProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FoxIDs.Infrastructure.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoxIDs.Client.Models.ViewModels
+{
+    public class TrackLinkUpPartyProfileViewModel : UpPartyProfileViewModel
+    {
+        [ListLength(Constants.Models.TrackLinkDownParty.SelectedUpPartiesMin, Constants.Models.TrackLinkDownParty.SelectedUpPartiesMax, Constants.Models.Party.NameLength, Constants.Models.TrackLinkDownParty.SelectedUpPartiesNameRegExPattern)]
+        [Display(Name = "Selected authentication methods (use * to select all authentication methods)")]
+        public List<string> SelectedUpParties { get; set; } = new List<string>(["*"]);
+    }
+}
---
+++ src/FoxIDs.ControlClient/Models/ViewModels/Parties/UpPartyProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoxIDs.Client.Models.ViewModels
+{
+    public abstract class UpPartyProfileViewModel
+    {
+        [Required]
+        [MaxLength(Constants.Models.Party.ProfileNameLength)]
+        [RegularExpression(Constants.Models.Party.NameRegExPattern)]
+        public string Name { get; set; }
+
+        [MaxLength(Constants.Models.Party.DisplayNameLength)]
+        [RegularExpression(Constants.Models.Party.DisplayNameRegExPattern)]
+        public string DisplayName { get; set; }
+    }
+}

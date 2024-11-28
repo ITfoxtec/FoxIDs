2b81031ac00aeb673af521db79602051ebe2aa43
Anders Revsgaard <anders@itfoxtec.com>
2024-11-28 13:56:19 +0100

    Can not be change to X509CertificateLoader LoadPkcs12 or LoadCertificate because it should automatically select between the two methods.

--- src/FoxIDs.Control/Controllers/Helpers/TReadCertificateController.cs
+++ src/FoxIDs.Control/Controllers/Helpers/TReadCertificateController.cs
@@ -37,8 +37,9 @@ namespace FoxIDs.Controllers
             {
                 var certificate = certificateAndPassword.Password.IsNullOrWhiteSpace() switch
                 {
-                    true => X509CertificateLoader.LoadPkcs12(WebEncoders.Base64UrlDecode(certificateAndPassword.EncodeCertificate), string.Empty, keyStorageFlags: X509KeyStorageFlags.Exportable),
-                    false => X509CertificateLoader.LoadPkcs12(WebEncoders.Base64UrlDecode(certificateAndPassword.EncodeCertificate), certificateAndPassword.Password, keyStorageFlags: X509KeyStorageFlags.Exportable),
+                    //Can not be change to X509CertificateLoader LoadPkcs12 or LoadCertificate because it should automatically select between the two methods.
+                    true => new X509Certificate2(WebEncoders.Base64UrlDecode(certificateAndPassword.EncodeCertificate), string.Empty, keyStorageFlags: X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable),
+                    false => new X509Certificate2(WebEncoders.Base64UrlDecode(certificateAndPassword.EncodeCertificate), certificateAndPassword.Password, keyStorageFlags: X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable),
                 };
 
                 if (!certificateAndPassword.Password.IsNullOrWhiteSpace() && !certificate.HasPrivateKey)

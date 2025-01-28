04eda8b1afd352c8e92ed86255a3391c8ee8fdd5
Anders Revsgaard <anders@itfoxtec.com>
2025-01-28 09:58:58 +0100

    Add RequestPath in OpenSearchEventLogItem

--- src/FoxIDs.Shared/Models/Tracks/Logs/Logging/OpenSearchEventLogItem.cs
+++ src/FoxIDs.Shared/Models/Tracks/Logs/Logging/OpenSearchEventLogItem.cs
@@ -3,6 +3,7 @@
     public class OpenSearchEventLogItem : OpenSearchLogItemBase
     {
         public string Message { get; set; }
+        public string RequestPath { get; set; }
         public string UserId { get; set; }
         public string Email { get; set; }
         public string UsageType { get; set; }

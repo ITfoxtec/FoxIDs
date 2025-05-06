2c95aa656754f570d06af16e7e809373c5a4583f
Anders Revsgaard <anders@itfoxtec.com>
2025-05-06 13:52:42 +0200

    Control Client support clipboard on http.

--- src/FoxIDs.ControlClient/wwwroot/js/functions.js
+++ src/FoxIDs.ControlClient/wwwroot/js/functions.js
@@ -11,6 +11,36 @@ function readError() {
     return document.getElementById('error').textContent;    
 }
 
+
+async function clipboardWriteText(text) {
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(text);
+    }
+    else {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        textArea.style.position = "absolute";
+        textArea.style.left = "0px";
+        textArea.style.right = "0px";
+        textArea.style.height = "0px";
+        textArea.style.width = "0px";
+
+        document.body.prepend(textArea);
+        textArea.select();
+
+        try {
+            document.execCommand('copy');
+        }
+        catch (error) {
+            console.error(error);
+        }
+        finally {
+            textArea.remove();
+        }
+    }
+}
+
 var mollie = null;
 var cardNumber = null;
 var cardHolder = null;

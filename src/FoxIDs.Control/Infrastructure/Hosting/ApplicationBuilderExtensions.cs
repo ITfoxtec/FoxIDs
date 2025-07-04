dffc9b2019226e7336a55608f72291cc56f5ac7f
Anders Revsgaard <anders@itfoxtec.com>
2025-07-04 17:38:31 +0200

    Clean Swagger V1 document to be as before.

--- src/FoxIDs.Control/Infrastructure/Hosting/ApplicationBuilderExtensions.cs
+++ src/FoxIDs.Control/Infrastructure/Hosting/ApplicationBuilderExtensions.cs
@@ -6,6 +6,11 @@ using System;
 using Microsoft.AspNetCore.Http;
 using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Readers;
+using Microsoft.OpenApi.Writers;
+using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace FoxIDs.Infrastructure.Hosting
 {
@@ -25,13 +30,33 @@ namespace FoxIDs.Infrastructure.Hosting
                     var result = await response.Content.ReadAsStringAsync();
 
                     // Replace to support Swagger V1
-                    result = result.Replace("\"version\": \"v2\"", "\"version\": \"v1\"");
                     result = result.Replace("{tenant_name}/{track_name}", "[tenant_name]/[track_name]");
+
+                    using var textReader = new StringReader(result);
+                    var document = new OpenApiTextReaderReader().Read(textReader, out var diagnostic);
+                    document.Info.Version = "v1";
+                    foreach(var path in document.Paths)
+                    {
+                        if (path.Key.StartsWith("/[tenant_name]/[track_name]"))
+                        {
+                            foreach(var op in path.Value.Operations)
+                            {
+                                op.Value.Parameters = op.Value.Parameters.Where(pa => pa.Name != "tenant_name" && pa.Name != "track_name").ToList();
+                            }
+                        }
+                    }
+
+                    var stringBuilder = new StringBuilder();
+                    using (var stringWriter = new StringWriter(stringBuilder))
+                    {
+                        var writer = new OpenApiJsonWriter(stringWriter);
+                        document.SerializeAsV3(writer);
+                    }
 
                     context.Response.StatusCode = (int)response.StatusCode;
                     context.Response.ContentType = response.Content.Headers.ContentType?.ToString() ?? "text/html";
 
-                    await context.Response.WriteAsync(result);
+                    await context.Response.WriteAsync(stringBuilder.ToString());
                 }
                 else
                 {

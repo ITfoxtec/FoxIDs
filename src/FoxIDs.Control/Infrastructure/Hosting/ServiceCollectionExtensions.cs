413fde94493ba490f31cdc2b10024736d9f0473f
Anders Revsgaard <anders@itfoxtec.com>
2025-08-22 11:58:03 +0200

    Change seed to use SeedBackgroundService

--- src/FoxIDs.Control/Infrastructure/Hosting/ServiceCollectionExtensions.cs
+++ src/FoxIDs.Control/Infrastructure/Hosting/ServiceCollectionExtensions.cs
@@ -39,7 +39,10 @@ namespace FoxIDs.Infrastructure.Hosting
             services.AddSharedLogic(settings);
 
             services.AddTransient<SendEventEmailLogic>();
+
             services.AddTransient<SeedLogic>();
+            services.AddHostedService<SeedBackgroundService>();
+
             services.AddTransient<MasterTenantDocumentsSeedLogic>();
             services.AddTransient<MainTenantDocumentsSeedLogic>();
 

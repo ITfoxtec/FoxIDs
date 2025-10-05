e1f5131c7ef9490a231367cad75185a302b1d61b
Anders Revsgaard <anders@itfoxtec.com>
2025-10-05 12:19:52 +0200

    LogOpenSearchLogic load event data correct

--- src/FoxIDs.Control/Logic/Logs/LogOpenSearchLogic.cs
+++ src/FoxIDs.Control/Logic/Logs/LogOpenSearchLogic.cs
@@ -276,8 +276,7 @@ namespace FoxIDs.Logic
 
             if (logRequest.QueryEvents)
             {
-                boolQuery = boolQuery.MustNot(m => m.Exists(e => e.Field(f => f.UsageType)));
-                boolQuery = boolQuery.MustNot(m => m.Exists(e => e.Field(f => f.AuditDataAction)));
+                boolQuery = boolQuery.MustNot(m => m.Exists(e => e.Field(f => f.UsageType)) || m.Exists(e => e.Field(f => f.AuditDataAction)));
             }
            
             boolQuery = boolQuery.Must(m =>

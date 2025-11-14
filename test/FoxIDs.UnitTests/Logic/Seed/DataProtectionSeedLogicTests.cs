b341a030e865ceddd420e6d9fcaa1ad16db4e09b
Anders Revsgaard <anders@itfoxtec.com>
2025-11-14 15:28:44 +0100

    DataProtectionSeedLogicTests corrected.

--- test/FoxIDs.UnitTests/Logic/Seed/DataProtectionSeedLogicTests.cs
+++ test/FoxIDs.UnitTests/Logic/Seed/DataProtectionSeedLogicTests.cs
@@ -1,6 +1,7 @@
 using FoxIDs.Logic.Seed;
 using Microsoft.AspNetCore.DataProtection;
 using Moq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -13,7 +14,7 @@ namespace FoxIDs.UnitTests.Logic.Seed
         public async Task EnsureKeysAsync_ProtectsDataUsingProvider()
         {
             var protector = new Mock<IDataProtector>();
-            protector.Setup(p => p.Protect(nameof(DataProtectionSeedLogic))).Returns(nameof(DataProtectionSeedLogic));
+            protector.Setup(p => p.Protect(It.IsAny<byte[]>())).Returns(System.Array.Empty<byte>());
 
             var provider = new Mock<IDataProtectionProvider>();
             provider.Setup(p => p.CreateProtector(nameof(DataProtectionSeedLogic))).Returns(protector.Object);
@@ -23,7 +24,7 @@ namespace FoxIDs.UnitTests.Logic.Seed
             await logic.EnsureKeysAsync(CancellationToken.None);
 
             provider.Verify(p => p.CreateProtector(nameof(DataProtectionSeedLogic)), Times.Once);
-            protector.Verify(p => p.Protect(nameof(DataProtectionSeedLogic)), Times.Once);
+            protector.Verify(p => p.Protect(It.Is<byte[]>(b => b != null && Encoding.UTF8.GetString(b) == nameof(DataProtectionSeedLogic))), Times.Once);
         }
     }
 }

7be65681944852c3075933e94edd01f3a63f5c5c
Anders Revsgaard <anders@itfoxtec.com>
2025-12-02 10:14:10 +0100

    Add validation to PasswordHistoryItem

--- src/FoxIDs.ControlShared/Models/Api/Tracks/PasswordHistoryItem.cs
+++ src/FoxIDs.ControlShared/Models/Api/Tracks/PasswordHistoryItem.cs
@@ -1,15 +1,26 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace FoxIDs.Models.Api
 {
-    public class PasswordHistoryItem
+    public class PasswordHistoryItem : IValidatableObject
     {
+        [Required]
         [MaxLength(Constants.Models.SecretHash.HashAlgorithmLength)]
         public string HashAlgorithm { get; set; }
 
+        [Required]
         [MaxLength(Constants.Models.SecretHash.HashLength)]
         public string Hash { get; set; }
 
         public long Created { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!HashAlgorithm.Equals("SHA256", System.StringComparison.Ordinal))
+            {
+                yield return new ValidationResult($"Unsupported {nameof(HashAlgorithm)} '{HashAlgorithm}'. Only 'SHA256' is supported for password history.", [nameof(HashAlgorithm)]);
+            }
+        }
     }
 }
--- src/FoxIDs.Shared/Models/Tracks/PasswordHistoryItem.cs
+++ src/FoxIDs.Shared/Models/Tracks/PasswordHistoryItem.cs
@@ -5,10 +5,12 @@ namespace FoxIDs.Models
 {
     public class PasswordHistoryItem
     {
+        [Required]
         [MaxLength(Constants.Models.SecretHash.HashAlgorithmLength)]
         [JsonProperty(PropertyName = "hash_algorithm")]
         public string HashAlgorithm { get; set; }
 
+        [Required]
         [MaxLength(Constants.Models.SecretHash.PasswordHistoryHashLength)]
         [JsonProperty(PropertyName = "hash")]
         public string Hash { get; set; }

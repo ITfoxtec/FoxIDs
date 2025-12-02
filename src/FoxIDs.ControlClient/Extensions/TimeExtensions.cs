cf4cf3e66789c78d598d7bcf9761ec9fb02ee555
Anders Revsgaard <anders@itfoxtec.com>
2025-12-02 16:43:08 +0100

    Correct date time and cert download in client.

--- src/FoxIDs.ControlClient/Extensions/TimeExtensions.cs
+++ src/FoxIDs.ControlClient/Extensions/TimeExtensions.cs
@@ -8,11 +8,11 @@ namespace FoxIDs.Client
         public static string TimeToString(this long time)
         {
             var dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(time);
-            return dateTimeOffset.ToLocalTime().ToString(CultureInfo.InvariantCulture);
+            return dateTimeOffset.ToLocalTime().ToString("dd/MM/yyyy HH:mm:ss");
         }
 
-        public static string ToDateText(this DateTime dateTime) => dateTime.ToString(CultureInfo.InvariantCulture);
+        public static string ToDateText(this DateTime dateTime) => dateTime.ToString("dd/MM/yyyy HH:mm:ss");
 
-        public static string ToDateText(this DateTimeOffset dateTimeOffset) => dateTimeOffset.ToString(CultureInfo.InvariantCulture);
+        public static string ToDateText(this DateTimeOffset dateTimeOffset) => dateTimeOffset.ToString("dd/MM/yyyy HH:mm:ss");
     }
 }
